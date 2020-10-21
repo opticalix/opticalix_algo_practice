dc19e6f5acfc6779e9a1b9569429b061355627ae
jihongfei <jihongfei@chinamobile.com>
2020-10-21 21:29:20 +0800

    Delete svm ml, add other problem

--- src/main/java/com/opticalix/problem/others/OtherProblem.java
+++ src/main/java/com/opticalix/problem/others/OtherProblem.java
@@ -1,5 +1,7 @@
 package com.opticalix.problem.others;
 
+import java.util.Scanner;
+
 /**
  * @author Felix
  * @date 07/09/2019 11:13 AM
@@ -37,6 +39,38 @@ public class OtherProblem {
             return mid;
         } else {
             return -1;
+        }
+    }
+
+    /**
+     * https://exercise.acmcoder.com/online/online_judge_ques?ques_id=1668&konwledgeId=136
+     */
+    public void buyStock() {
+        Scanner sc = new Scanner(System.in);
+        //1 2 1 2 3 2 3 4 5 4
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            int ret = 1;
+            Counter cnt = new Counter();
+
+            for (int i = 1; i < n; i++) {
+                if (--cnt.k < 0) {
+                    cnt.reset();
+                    ret--;
+                } else {
+                    ret++;
+                }
+            }
+            System.out.println(ret);
+        }
+    }
+
+    static class Counter {
+        public int k = 1;
+        public int loop = 1;
+        public void reset() {
+            loop++;
+            k = loop;
         }
     }
 
